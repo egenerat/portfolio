71cb0eef6a891f80e08b17e12107cc0d5a611d75
egenerat <emile.generat@outlook.com>
2017-10-09 22:49:41 +0100

    Calculate metrics

--- business.js
+++ business.js
@@ -5,6 +5,8 @@ exports.filter = (data) => {
 		delete element.sectors;
 		delete element.marketCapitalizations;
 		delete element.regions;
+		element.pePb = (element.pe * element.pb).toFixed(1);
+		element.forwardPePb = (element.forwardPe * element.forwardPb).toFixed(1);
 	});
 	return Promise.resolve(data);
 }
