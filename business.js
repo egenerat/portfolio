e26e4cee768c766341c3e9d876558061a0c07323
egenerat <emile.generat@outlook.com>
2017-10-13 21:20:21 +0100

    Improve business layer to compute statistics on holdings

--- business.js
+++ business.js
@@ -1,12 +1,23 @@
+function sumTop10Holdings(top10Holdings) {
+	let result = 0;
+	top10Holdings.forEach( function(element) {
+		// ["SYMBOL", "Company Ltd", "5.23%"]
+		result += parseFloat(element[2].replace('%', ''));
+	});
+	return result;
+}
+
 exports.filter = (data) => {
 	data.forEach( function(element) {;
+		element.pePb = (element.pe * element.pb).toFixed(1);
+		element.forecastPePb = (element.forecastPe * element.forecastPb).toFixed(1);
+		element.percentageTop10 = sumTop10Holdings(element.top10Holdings);
 		delete element.stockStyleExposure;
 		delete element.stockStyleExposure;
 		delete element.sectors;
 		delete element.marketCapitalizations;
 		delete element.regions;
-		element.pePb = (element.pe * element.pb).toFixed(1);
-		element.forwardPePb = (element.forwardPe * element.forwardPb).toFixed(1);
+		delete element.top10Holdings;
 	});
 	return Promise.resolve(data);
 }
