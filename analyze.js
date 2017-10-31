7eab37fe13688e177cd2a62ef3ce71931459c862
egenerat <emile.generat@outlook.com>
2017-10-31 19:36:20 +0000

    Promise - Parse the export csv

--- analyze.js
+++ analyze.js
@@ -6,11 +6,20 @@ utils.list_files(OUTPUT_FOLDER)
     .then(console.log);
 
 const constructFilePath = (date) => {
-    return OUTPUT_FOLDER + date + ".csv";
+    return Promise.resolve(OUTPUT_FOLDER + date + ".csv");
 };
 
-let path = ["2017-10-21", "2017-10-28"];
-path.forEach(x => {
-    let path = constructFilePath(x);
-    utils.readFromCsv(path);
-});
+const computeChangesEtf = (resultList) => {
+    // resultList.reduce(() => );
+    // forEach(x => console.log(x.rates.EUR));
+    for (const currency of ["EEM"]) {
+        let variation = resultList[1].rates[currency] / resultList[0].rates[currency];
+        console.log("GBP/" + currency + ": " + variation.toFixed(2));
+    }
+};
+
+let path = ["test-2017-10-29", "test-2017-10-30"];
+Promise.all(path
+    .map((x) => constructFilePath(x)
+        .then(utils.readFromCsv)
+        .then(computeChangesEtf)));
