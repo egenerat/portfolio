181c9ba2aa416cc124d5fa7f980564ad0e14944a
egenerat <emile.generat@outlook.com>
2017-11-08 22:35:19 +0000

    Financials

--- financials.js
+++ financials.js
@@ -1,11 +1,17 @@
 "use strict";
 module.exports.computeForwardPeRatio = (currentPeRatio, earningsGrowth)=> {
+    // TODO not implemented yet
     let forwardPeRatio = 0;
     return 0;
 };
 
 module.exports.haveEarningsChanged = (oldPrice, oldPe, newPrice, newPe) => {
-    let oldEarnings = oldPe / oldPrice;
-    let newEarnings = newPe / newPrice;
-    return oldEarnings !== newEarnings;
+    let oldEps = oldPrice / oldPe;
+    let newEps = newPrice / newPe;
+    return oldEps !== newEps;
+};
+
+module.exports.hasPeChanged = (oldPe, newPe) => {
+    const margin = 0.01;
+    return newPe < (1 - margin) * oldPe || newPe > (1 + margin) * oldPe;
 };
