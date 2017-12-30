978cb0af9834d3212a67c685ead0aa71cf5dffb5
Emile GENERAT <emile.generat@outlook.com>
2017-12-30 14:12:12 +0000

    Restructure analyser code, improvements and unit tests added

--- analyzer.js
+++ analyzer.js
@@ -1,104 +1,38 @@
 "use strict";
-const utils = require("./utils");
-const financials = require("./financials");
-const constants = require("./constants");
+
+const analyzerUtils = require("./analyzerUtils");
 const cli = require("cli");
-var colors = require("colors");
+const utils = require("./utils");
 
 const cliArgs = cli.parse({
-    criteria: [ "c", "Criteria to be used for filtering/ordering", "string", "pe" ],  //     --work STRING What kind of work to do
-    begin: [ "b", "First date", "string"],//, "2017-09-24" ],
-    end: [ "e", "End date", "string"]//, "2017-11-25" ],
+    criteria: [ "c", "Criteria to be used for filtering/ordering", "string", "pe" ], //--criteria STRING
+    begin: [ "b", "First date", "string"],
+    end: [ "e", "End date", "string"],
+    threshold: [ "t", "Threshold", "int", 0], // --threshold 5, to display only changes higher than 5%
 });
+
+const checkCliArguments = (cliArgs) => {
+    let result = true;
+    if (!cliArgs.begin) {
+        cli.error("--begin date required");
+        result = false;
+    }
+    
+    if (!cliArgs.end) {
+        cli.error("--end date required");
+        result = false;
+    }
+    return result;
+};
+
 let path = [cliArgs.begin, cliArgs.end];
-const CRITERIA = cliArgs.criteria;
-// utils.list_files(OUTPUT_FOLDER)
-//     .then(console.log);
+const criteria = cliArgs.criteria;
 
-const constructFilePath = (date) => {
-    return Promise.resolve(constants.EXPORT_FOLDER + date + ".csv");
-};
-
-const computeChangesEtf = (resultList) => {
-    let cheaperList = [];
-    let moreExpensiveList = [];
-    let tickerList = resultList[0].reduce( (total, elt) => {
-        total.push(elt.name);
-        return total;
-    }, []);
-    for (const ticker of tickerList) {
-        // TODO should be compared over tickers, but were not available in oldest datasets
-        let old = resultList[0].find(x => x.name === ticker);
-        let n = resultList[1].find(x => x.name === ticker);
-        if (old !== undefined && n !== undefined) {
-            // let earningsUpdate = financials.haveEarningsChanged(old.price, old.pe, n.price, n.pe);
-            // console.log(ticker + " " + priceVariation);
-            const oldValue = parseFloat(old[CRITERIA]);
-            const newValue = parseFloat(n[CRITERIA]);
-            let listToAppend;
-            if (!financials.areFloatEqual(oldValue, newValue)) {
-                let variation = newValue / oldValue;
-                if (variation < 1) {
-                    listToAppend = cheaperList;
-                }
-                else {
-                    listToAppend = moreExpensiveList;
-                }
-                listToAppend.push({
-                        old: old,
-                        n: n,
-                        variation: variation
-                    });
-                }
-            }
-        }
-    cheaperList.sort( (a, b) => {
-        return a.variation - b.variation;
-    });
-    moreExpensiveList.sort( (a, b) => {
-        return b.variation - a.variation;
-    });
-    return {
-        cheaperList: cheaperList,
-        moreExpensiveList: moreExpensiveList,
-        criteria: CRITERIA
-    };
-};
-
-const displaySubList = (subList, criteria) => {
-    let old, n, variation;
-    for (let e of subList) {
-        ({ old, n, variation } = e);
-        let priceVariation = utils.prettyPrintPercentage(n.price / old.price);
-        console.log(n.name + " " + priceVariation);
-        console.log(old[criteria] + " => " + n[criteria]);
-        if (variation < 1) {
-            console.log(`↘ ${utils.prettyPrintPercentage(variation)}`.green);
-        }
-        else {
-            console.log(`↗ ${utils.prettyPrintPercentage(variation)}`.red);
-        }
-    }
-};
-
-const displayResults = (res) => {
-    let cheaperList, moreExpensiveList, criteria;
-    ({ cheaperList, moreExpensiveList, criteria } = res);
-
-    console.log(`Based on ${criteria}`);
-    if (cheaperList.length > 0) {
-        console.log("============================\nCheaper\n");
-    displaySubList(cheaperList, criteria);
-    }
-    if (moreExpensiveList.length > 0) {
-        console.log("============================\nMore expensive");
-    displaySubList(moreExpensiveList, criteria);
-    }
-};
-
-Promise.all(path
-    .map((x) => constructFilePath(x)
-        .then(utils.readFromCsv)))
-    .then(computeChangesEtf)
-    .then(displayResults)
-    .catch(console.err);
+if (checkCliArguments(cliArgs) ) {
+    Promise.all(path
+        .map((x) => analyzerUtils.constructFilePath(x)
+            .then(utils.readFromCsv)))
+        .then(res => analyzerUtils.computeChangesEtf(res, criteria))
+        .then(res => analyzerUtils.displayResults(res, criteria, cliArgs.threshold))
+        .catch(cli.error);
+}
---
+++ analyzerUtils.js
@@ -0,0 +1,102 @@
+"use strict";
+
+const colors = require("colors");
+const constants = require("./constants");
+const financials = require("./financials");
+const utils = require("./utils");
+
+const constructFilePath = (date) => {
+    return Promise.resolve(constants.EXPORT_FOLDER + date + ".csv");
+};
+
+const computeChangesEtf = ([oldSecurities, newSecurities], criteria) => {
+    let cheaperList = [];
+    let moreExpensiveList = [];
+    let tickerList = oldSecurities.reduce( (total, elt) => {
+        total.push(elt.name);
+        return total;
+    }, []);
+    for (const ticker of tickerList) {
+        // TODO should be compared over tickers, but were not available in oldest datasets
+        let old = oldSecurities.find(x => x.name === ticker);
+        let n = newSecurities.find(x => x.name === ticker);
+        if (old !== undefined && n !== undefined) {
+            let oldValue = old[criteria];
+            let newValue = n[criteria];
+            if ( !isNaN(oldValue) && !isNaN(newValue) ) {
+                oldValue = parseFloat(old[criteria]);
+                newValue = parseFloat(n[criteria]);
+                let listToAppend;
+                if (!financials.areFloatEqual(oldValue, newValue)) {
+                    let variation = newValue / oldValue;
+                    if (variation < 1) {
+                        listToAppend = cheaperList;
+                    }
+                    else {
+                        listToAppend = moreExpensiveList;
+                    }
+                    listToAppend.push({
+                        old: old,
+                        n: n,
+                        variation: variation
+                    });
+                }
+            }
+        }
+    }
+    cheaperList.sort( (a, b) => {
+        return a.variation - b.variation;
+    });
+    moreExpensiveList.sort( (a, b) => {
+        return b.variation - a.variation;
+    });
+    return {
+        cheaperList: cheaperList,
+        moreExpensiveList: moreExpensiveList
+    };
+};
+
+const displaySubList = (subList, criteria, threshold) => {
+    let old, n, variation;
+    for (let e of subList) {
+        ({ old, n, variation } = e);
+        let priceVariation = utils.prettyPrintPercentage(n.price / old.price);
+        // Only display changes over the threshold
+        if ( Math.abs(variation - 1) * 100 > threshold ) {
+            console.log(n.name + " " + priceVariation);
+            console.log(old[criteria] + " => " + n[criteria]);
+            if (variation < 1) {
+                console.log(`↘ ${utils.prettyPrintPercentage(variation)}`.green);
+            }
+            else {
+                console.log(`↗ ${utils.prettyPrintPercentage(variation)}`.red);
+            }
+        }
+    }
+};
+
+const displayResults = (res, criteria, threshold) => {
+    let cheaperList, moreExpensiveList;
+    ({ cheaperList, moreExpensiveList } = res);
+
+    console.log(`Based on ${criteria}`);
+    if (cheaperList.length > 0) {
+        console.log("\n============================\nCheaper\n");
+        displaySubList(cheaperList, criteria, threshold);
+    }
+    else {
+        console.log("∅");
+    }
+    if (moreExpensiveList.length > 0) {
+        console.log("\n============================\nMore expensive\n");
+        displaySubList(moreExpensiveList, criteria, threshold);
+    }
+    else {
+        console.log("∅");
+    }
+};
+
+module.exports.constructFilePath = constructFilePath;
+module.exports.computeChangesEtf = computeChangesEtf;
+module.exports.displaySubList = displaySubList;
+module.exports.displayResults = displayResults;
