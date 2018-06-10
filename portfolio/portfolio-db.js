5def1cc35d59c0a42847a94cea0516014f34768b
egenerat <emile.generat@outlook.com>
2018-06-10 23:26:34 +0100

    Portfolio: initial commit

---
+++ portfolio/portfolio-db.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const { PortfolioSecurity } = require("./portfolio_security");
+const { calculateAverageMetric } = require("./portfolio");
+
+const analyzerUtils = require("../analyzerUtils");
+const utils = require("../utils");
+
+const portfolio = [];
+portfolio.push(new PortfolioSecurity("EEM", 10));
+portfolio.push(new PortfolioSecurity("EWS", 10));
+portfolio.push(new PortfolioSecurity("EWZ", 10));
+
+const INTERESTING_METRICS = ["pe", "forecastPe"];
+
+const path = "2018-1-6";
+analyzerUtils.constructFilePath(path)
+    .then(utils.readFromCsv)
+    .then(fundamentals => INTERESTING_METRICS.forEach(metric => 
+        calculateAverageMetric(portfolio, fundamentals, metric)
+    ));
