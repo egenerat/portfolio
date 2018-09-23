65ecd72b9b2f936dee337db96aa527bc746addb6
egenerat <emile.generat@outlook.com>
2018-09-23 22:40:34 +0100

    Singleton DB

--- db.js
+++ db.js
@@ -1,14 +1,33 @@
 const Nedb = require("nedb");
 
+const DB_FOLDER = "data/db/";
+
+const instances = {};
+
 class Database {
 
-    constructor(dbName) {
-        this.securities = new Nedb({ filename: `data/output/${dbName}`, autoload: true });
+    constructor(filename) {
+        filename = filename || "data.db";
+        const db_path = DB_FOLDER + filename;
+        this.db = this.getInstance(db_path);
     }
 
-    find(filter) {
+    getInstance(db_path) {
+        if (instances[db_path]) {
+            console.log("retrieve instance");
+            return instances[db_path];
+        }
+        console.log("create new instance");
+        instances[db_path] = new Nedb({ filename: db_path, autoload: true });
+        return instances[db_path];
+    }
+
+    insert(obj) {
+        if ( Array.isArray(obj) ) {
+            obj = obj.filter(x => x !== undefined);
+        }
         return new Promise((resolve, reject) => {
-            this.securities.find(filter, (err, docs) => {
+            this.db.insert(obj, (err, docs) => {
                 if (err) {
                     reject(err);
                 } else {
@@ -18,12 +37,9 @@ class Database {
         });
     }
 
-    insert(obj) {
-        if (Array.isArray(obj)) {
-            obj = obj.filter(x => x !== undefined);
-        }
+    find(filter) {
         return new Promise((resolve, reject) => {
-            this.securities.insert(obj, (err, docs) => {
+            this.db.find(filter, (err, docs) => {
                 if (err) {
                     reject(err);
                 } else {
