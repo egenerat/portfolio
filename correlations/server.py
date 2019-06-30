0636d474764c1dd6f795ebf5c4f73e2a101ae023
egenerat <emile.generat@outlook.com>
2019-06-30 22:15:00 +0100

    Improve API robustness Case where no JSON is sent

--- correlations/server.py
+++ correlations/server.py
@@ -7,26 +7,32 @@ app = Flask(__name__)
 
 @app.route("/correlations", methods=['POST'])
 def correlation_api():
-    req_json = request.get_json()
-    perf_list = parse_performances_from_dict(req_json)
-    if len(perf_list) < 2:
-        return jsonify({
-            'error': 'not enough valid data'
-        }), 400
     try:
+        req_json = request.get_json()
+        valid_input = True
+        perf_list = []
+        if req_json:
+            perf_list = parse_performances_from_dict(req_json)
+            if len(perf_list) < 2:
+                valid_input = False
+        else:
+            valid_input = False
+        if not valid_input:
+            return jsonify({
+                'error': 'not enough valid data'
+            }), 400
         corr, min_size, limiting = correlations(perf_list)
+        data = {
+            'correlations': corr,
+            'min_size': min_size,
+            'limiting': limiting
+        }
+        return jsonify(data)
     except Exception:
         traceback.print_exc()
         return jsonify({
             'error': 'Internal error'
         }), 500
-
-    data = {
-        'correlations': corr,
-        'min_size': min_size,
-        'limiting': limiting
-    }
-    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
