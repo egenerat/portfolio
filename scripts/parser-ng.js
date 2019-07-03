b07aaa06712c9ccb681b59e168beb88e0db3a7ee
egenerat <emile.generat@outlook.com>
2019-07-03 21:24:50 +0100

    --save option adeed to parser-ng. Closes #30

--- scripts/parser-ng.js
+++ scripts/parser-ng.js
@@ -2,29 +2,41 @@
 
 "use strict";
 
+const cli = require("cli");
 const { Database } = require("../app/persistence/db.js");
 const { parsePage } = require("../app/parsers/parser-utils.js");
 const logger = require("../app/core/logger.js");
 const utils = require("../app/core/utils.js");
 
-
 const portfolioDb = new Database("portfolio.db");
 const securitiesDb = new Database("securities.db");
 const today = utils.formatDate(new Date());
 
-const args = process.argv.slice(2);
+const cliArgs = cli.parse({
+    save: [ "s", "save", "boolean"],
+});
+
+let save = cliArgs.save;
+const args = cli.args;
+
 if (args.length > 0) {
     args.forEach(url => 
         parsePage(url)
             .then(x => {
-                x.date = today;
-                if (x.portfolioName) {
-                    portfolioDb.insert(x);
+                if (x !== undefined) {
+                    x.date = today;
+                    if (save) {
+                        if (x.portfolioName) {
+                            portfolioDb.insert(x);
+                        }
+                        else {
+                            securitiesDb.insert(x);
+                        }
+                    }
+                    else {
+                        logger.log(x);
+                    }
                 }
-                else {
-                    securitiesDb.insert(x);
-                }
-                
             })
             .catch(err => logger.error(`${url}: ${err}`))
     );
