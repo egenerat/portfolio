1accb65ebf39e8d78c6a3a16233572cf34b93c26
egenerat <emile.generat@outlook.com>
2019-07-15 00:52:48 +0100

    minor bugfix test syntax

--- test/tests-parser-utils.js
+++ test/tests-parser-utils.js
@@ -3,7 +3,7 @@ const parserUtils = require("../app/parsers/parser-utils.js");
 const assert = require("assert");
 
 describe("parser utils", () => {
-    it("aggregate", () => {
+    describe("aggregate", () => {
         it("should aggregate list of dictionaries", () => {
             assert.deepEqual({a: 1, b: 2}, parserUtils.aggregate([{a: 1}, {b: 2}]));
         });
