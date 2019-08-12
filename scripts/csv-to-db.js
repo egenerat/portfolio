b689a7532d902c1f4673b6b3ef6468e43d045043
Emile Generat <egenerat@users.noreply.github.com>
2019-08-12 08:17:09 +0100

    CSV parser error handling (#60)
    Resolves #58

--- scripts/csv-to-db.js
+++ scripts/csv-to-db.js
@@ -1,15 +1,30 @@
 #! /usr/bin/env node
 "use strict";
 
+const cli = require("cli");
 const constants = require("../app/config/constants/constants.js");
+const logger = require("../app/core/logger.js");
 const utils = require("../app/core/utils.js");
 const { Database } = require("../app/persistence/db.js");
+
+const cliArgs = cli.parse({
+    save: [ "s", "Save output to database", "boolean" ],
+});
 
 const db = new Database("csv-import.db");
 utils.list_files(constants.EXPORT_FOLDER)
     .then(files =>
         files.map(filename => {
-            utils.readFromCsv(`${constants.EXPORT_FOLDER}/${filename}`)
-                .then(x => db.insert(x));
+            utils.readFromCsv(`${constants.EXPORT_FOLDER}${filename}`)
+                .then(x => {
+                    logger.info(`Extracted from file ${filename}`);
+                    if (cliArgs.save) {
+                        db.insert(x);
+                    }
+                    else {
+                        logger.info(x);
+                    }
+                })
+                .catch(logger.error);
         })
     );
